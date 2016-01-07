04e42aa420fa6dc28300ca4fe551861895a54bf5
Fissal <fissalalsharef@gmail.com>
2016-01-07 07:07:38 +0200

    Debugging

--- Assignment/forms.py
+++ Assignment/forms.py
@@ -27,8 +27,8 @@ class CoursesInfo(forms.Form):
     C_Teacher = forms.ModelChoiceField(queryset = Teacher.objects.all())
 
 
-courses = [(course.C_Name,course.C_Name) for course in Courses.objects.all()]
-students = [(student.Student_Name,student.Student_Name) for student in Student.objects.all()]
 class EnrollForm(forms.Form):
+    courses = [(course.C_Name,course.C_Name) for course in Courses.objects.all()]
+    students = [(student.Student_Name,student.Student_Name) for student in Student.objects.all()]
     courses = forms.ChoiceField(courses, required=False,widget=forms.Select())
     students =forms.ChoiceField(students,required=False,widget=forms.Select())
